c1be81397c5b605e25edb4c3f89ec80168de1796
Chhitij91 <srivastava.ch@spaceinsolutions.com>
2021-06-26 23:18:00 +0530

    Done deployment of ml usng flask

--- deployment/deployment/main.py
+++ deployment/deployment/main.py
@@ -1,8 +1,13 @@
 from logging import debug
-from flask import Flask , render_template
+from flask import Flask , render_template , request
+import joblib
 
 # behind the seen file will run main.py
 app = Flask(__name__)
+
+# load the modal 
+
+model = joblib.load('hiring_model.pkl')
 
 @app.route('/')
 def welcome():
@@ -15,6 +20,20 @@ def contact():
 @app.route('/feedback')
 def feedback():
     return 'feedBack Page'
+
+@app.route('/predict', methods= ['POST'])
+def predict():
+    exp = request.form.get('experience')
+    score = request.form.get('test_score')
+    interview_score = request.form.get('interview_score')
+
+    prediction = model.predict([[int(exp),int(score),int(interview_score)]])
+
+    print(prediction)
+
+    output = round(prediction[0],2)
+    
+    return render_template('base.html', prediction_text= f'Employee Salary will be $ {output}')
 
 
 # debug = true for every change it will  run live watch mode
